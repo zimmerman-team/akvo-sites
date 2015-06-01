442bbac5f5a570760a304b414fce59470407dc3f
Daan <daancjanssen@gmail.com>
2015-06-01 17:15:31 +0200

    map location

--- templates/content-page.php
+++ templates/content-page.php
@@ -11,7 +11,17 @@ else {
 
 	<div class="bg">
         <div class="main-image">
-          <?php the_post_thumbnail( 'large' ); ?>
+        <?php the_post_thumbnail( 'large' ); ?>
+        <?php
+            $map = get_post_meta( get_the_ID(), '_map_option_address', true );
+            if(!empty($map)) {
+                flexmap_show_map(array(
+                    'width' => '100%',
+                    'height' => '400px',
+                    'address' => $map
+                ));
+            } 
+        ?>
         </div>
 
         <div class="row">
@@ -23,16 +33,6 @@ else {
                 </div>
             </div>
         </div>
-        <?php
-    $map = get_post_meta( get_the_ID(), '_map_option_address', true );
-    if(!empty($map)) {
-        flexmap_show_map(array(
-            'width' => '100%',
-            'height' => '400px',
-            'address' => $map
-        ));
-    } 
-    ?>
 	</div>
 </div>
 
