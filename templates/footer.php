5d26a3eaea0ec396b38516e3238d9607b50ec044
Daan <daan@zimmermanzimmerman.nl>
2015-06-08 15:28:28 +0200

    footer fix

--- templates/footer.php
+++ templates/footer.php
@@ -1,32 +1,20 @@
 <footer class="content-info" role="contentinfo">
 	<div class="twitter">
 		<div class="container">
-			<?php dynamic_sidebar('sidebar-footer-high'); ?>
+			<div class="row">
+				<div class="col-md-6 col-offset-md-3">
+					<?php dynamic_sidebar('sidebar-footer-high'); ?>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="custom">
 		<div class="container">
-			<?php dynamic_sidebar('sidebar-footer'); ?>
-			<!-- <div class="newsletter">
-				<h4>Subscribe to our newsletter</h4>
-				<div class="col-md-6 col-md-offset-3">
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Your email address">
-						<span class="input-group-btn">
-							<button class="btn btn-default" type="button">Subscribe</button>
-						</span>
-					</div>
+			<div class="row">
+				<div class="col-md-6 col-offset-md-3">
+					<?php dynamic_sidebar('sidebar-footer'); ?>
 				</div>
-			</div> -->
-			<!-- <div class="socialmedia">
-				<div class="col-md-12">
-					<h4>Follow us</h4>
-					<i class="fa fa-youtube"></i>
-					<i class="fa fa-google-plus"></i>
-					<i class="fa fa-facebook"></i>
-					<i class="fa fa-twitter"></i>
-				</div>
-			</div> -->
+			</div>
 		</div>
 	</div>
 	<div class="fixed">
